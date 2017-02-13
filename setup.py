5432255e836be99ef75a991b670acad07d1892a1
peerster <paer.berge@gmail.com>
2017-02-13 21:16:25 +0100

    Birth of setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='genmenu',
+      version='0.1',
+      description='A meal menu planner',
+      url='https://github.com/peerster/genmenu',
+      author='Pär Berge',
+      author_email='paer.berge@gmail.com',
+      license='MIT',
+      packages=['genmenu'],
+      zip_safe=False)

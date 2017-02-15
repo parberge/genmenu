cbd58937fcb51f65022c1fac36c6ee8942430ac9
peerster <paer.berge@gmail.com>
2017-02-15 20:48:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='genmenu',
-      version='0.1',
+      version='0.1.1',
       description='A meal menu planner',
       url='https://github.com/peerster/genmenu',
       author='Pär Berge',

a64c633db6770f47cc82b9df03fa13ca1803081e
peerster <paer.berge@gmail.com>
2017-03-01 20:29:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup
 from codecs import open
 
-with open('README.md', 'r', 'utf-8') as f:
+with open('README.rst', 'r', 'utf-8') as f:
         readme = f.read()
 
 setup(name='genmenu',
-      version='0.1.3',
+      version='0.1.8',
       description='A meal menu planner',
       long_description=readme,
       url='https://github.com/peerster/genmenu',
